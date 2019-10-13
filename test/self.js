6858108d9798f122eae32d749c45a7822d0bd524
Vincent Weevers <mail@vincentweevers.nl>
2019-10-13 09:42:05 +0200

    Clone additionalMethods to prevent mutation
    Closes #3

--- test/self.js
+++ test/self.js
@@ -3,9 +3,11 @@
 var test = require('tape')
 var supports = require('..')
 var shape = require('./shape')
+var cloneable = require('./cloneable')
 
 test('no options', function (t) {
   shape(t, supports())
+  cloneable(t, supports())
   t.end()
 })
 
@@ -44,13 +46,30 @@ test('truthy options', function (t) {
 
 test('merges input objects without mutating them', function (t) {
   var input1 = { bufferKeys: null, streams: false }
-  var input2 = { streams: true }
+  var input2 = { streams: true, additionalMethods: {} }
   var manifest = supports(input1, input2)
 
+  manifest.foobar = true
+  manifest.additionalMethods.baz = true
+
   t.same(input1, { bufferKeys: null, streams: false })
-  t.same(input2, { streams: true })
+  t.same(input2, { streams: true, additionalMethods: {} })
   t.is(manifest.bufferKeys, false)
   t.is(manifest.streams, true)
   shape(t, manifest)
+  t.end()
+})
+
+test('inherits additionalMethods', function (t) {
+  var manifest = supports({ additionalMethods: { foo: true } }, {})
+  t.same(manifest.additionalMethods, { foo: true })
+  t.end()
+})
+
+test('does not merge additionalMethods', function (t) {
+  var input1 = { additionalMethods: { foo: true } }
+  var input2 = { additionalMethods: { bar: true } }
+  var manifest = supports(input1, input2)
+  t.same(manifest.additionalMethods, { bar: true })
   t.end()
 })
